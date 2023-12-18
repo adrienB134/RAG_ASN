39ec939b21d79282923e2446216458a44ec8fd4f
adrienB134 <102990337+adrienB134@users.noreply.github.com>
2023-12-18 18:42:41 +0100

    Better ingestion

--- Ingestion/ingest.py
+++ Ingestion/ingest.py
@@ -1,73 +1,103 @@
 """Load PDF from files, clean up, split, ingest into a vectorstore"""
-import pickle
+import os, logging
 import time
 from dotenv import load_dotenv
 
-from langchain.document_loaders import PyPDFDirectoryLoader
-from langchain.embeddings import OpenAIEmbeddings, FakeEmbeddings
-from langchain.embeddings.sentence_transformer import SentenceTransformerEmbeddings
+from langchain.embeddings import HuggingFaceInferenceAPIEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.vectorstores.faiss import FAISS
-from langchain.vectorstores.vectara import Vectara
 from langchain.vectorstores.chroma import Chroma
-
-
-# def ingest_docs():
-#     load_dotenv()
-#     for folder in range(0, 100):
-#         loader = PyPDFDirectoryLoader(f"ASN/lettres_de_suivi/{folder}")
-#         raw_documents = loader.load()
-#         text_splitter = RecursiveCharacterTextSplitter(
-#             chunk_size=3000,
-#             chunk_overlap=200,
-#         )
-#         documents = text_splitter.split_documents(raw_documents)
-#         embeddings = OpenAIEmbeddings()
-#         vectorstore = FAISS.from_documents(documents, embeddings)
-#         vectorstore.save_local("faiss_index")
-#         print(f"Folder {folder} done")
+from langchain.document_loaders import UnstructuredFileLoader
+from unstructured.cleaners.core import (
+    clean_extra_whitespace,
+    replace_mime_encodings,
+    replace_unicode_quotes,
+)
 
 
 def ingest_docs():
     load_dotenv()
 
-    for folder in range(0, 3):
-        loader = PyPDFDirectoryLoader(f"ASN/lettres_de_suivi/{folder}")
-        raw_documents = loader.load()
-        text_splitter = RecursiveCharacterTextSplitter(
-            chunk_size=3000,
-            chunk_overlap=200,
-        )
-        documents = text_splitter.split_documents(raw_documents)
-        embeddings = OpenAIEmbeddings()
-        vectorstore_db = Chroma.from_documents(
-            documents,
-            embeddings,
-            persist_directory="./chroma_db",
-            collection_name="lettres_de_suivi",
-        )
-        print(f"Folder {folder} done")
+    for folder in range(1, 2):
+        folder_start = time.time()
+        for file in os.listdir(f"ASN/lettres_de_suivi/{folder}"):
+            start_time = time.time()
+            attempts = 0
+            success = False
+            while attempts < 3 and not success:
+                try:
+                    print(" ")
+                    print(f"File: {file}, Attempt n° {attempts}")
+                    loader = UnstructuredFileLoader(
+                        f"ASN/lettres_de_suivi/{folder}/{file}",
+                        # mode="elements",
+                        post_processors=[
+                            clean_extra_whitespace,
+                            replace_unicode_quotes,
+                            replace_mime_encodings,
+                        ],
+                        # strategy="ocr_only",
+                    )
+                    raw_documents = loader.load()
+
+                    unrecognized_char = False
+                    for doc in raw_documents:
+                        for char in doc.page_content:
+                            if char == "\uFFFD":
+                                unrecognized_char = True
+                                print(f"{char}")
+                                break
+
+                    if unrecognized_char:
+                        print(f"Unrecognized characters, switching to OCR.")
+                        loader = UnstructuredFileLoader(
+                            f"ASN/lettres_de_suivi/{folder}/{file}",
+                            # mode="elements",
+                            post_processors=[
+                                clean_extra_whitespace,
+                                replace_unicode_quotes,
+                                replace_mime_encodings,
+                            ],
+                            strategy="ocr_only",
+                        )
+                        raw_documents = loader.load()
+
+                    text_splitter = RecursiveCharacterTextSplitter(
+                        chunk_size=1500,
+                        chunk_overlap=200,
+                    )
+                    documents = text_splitter.split_documents(raw_documents)
+
+                    embeddings_model = HuggingFaceInferenceAPIEmbeddings(
+                        api_key=os.environ["HF_TOKEN"],
+                        api_url="https://pikmjtam1n1c2rzu.us-east-1.aws.endpoints.huggingface.cloud",
+                        model_name="WhereIsAI/UAE-Large-V1",
+                    )
+
+                    for i in range(0, len(documents), 32):
+                        vectorstore_db = Chroma.from_documents(
+                            documents,
+                            embeddings_model,
+                            persist_directory="./chroma_db",
+                            collection_name="lettres_de_suivi",
+                        )
+
+                    print(
+                        f"Success! File {file} done in {round(time.time() - start_time,2)} s!"
+                    )
+                    success = True
+
+                except Exception as exception:
+                    logging.warning(f"Failure n° {attempts}, {exception}")
+                    attempts += 1
+                    if attempts == 3:
+                        with open("failures_list.txt", "a") as f:
+                            f.write(f"{file}\n")
+                        break
+
+        print(" ---- ")
+        print(f"Folder {folder} done in {round((time.time() - folder_start)/60, 2)}min")
 
     print("There are", vectorstore_db._collection.count(), "in the collection")
-
-
-# def ingest_docs():
-#     load_dotenv()
-#     a = 0
-#     for folder in range(0, 370):
-#         loader = PyPDFDirectoryLoader(f"ASN/lettres_de_suivi/{folder}")
-#         raw_documents = loader.load()
-#         text_splitter = RecursiveCharacterTextSplitter(
-#             chunk_size=10000,
-#             chunk_overlap=200,
-#         )
-#         documents = text_splitter.split_documents(raw_documents)
-#         Vectara.from_documents(
-#             documents,
-#             FakeEmbeddings(size=10000),
-#         )
-#         print(f"Folder {folder} done")
-#         time.sleep(5)
 
 
 if __name__ == "__main__":
